e62d5d938cbb07960898fe4b023469769cc7bf6b
Max Putas <max@duolingo.com>
2014-07-27 19:38:11 -0400

    Moved from a class-based model to a module-based one

--- test.py
+++ test.py
@@ -7,55 +7,45 @@ import nose
 testcenter_url = 'http://testcenter.duolingo.com/'
 languages = ("en", "es", "fr", "de", "it", "pt", "ru", "hi", "hu", "tr")
 
-#args = sys.argv[1:]
-#sys.argv = sys.argv[0:1]
+browserName = sys.argv[1]
+systemPlatform = sys.argv[2]
 
+capabilities = {
+  "browserName": browserName,
+  "platform": systemPlatform,
+}
+
+# Setup a browser session for the chosen OS/browser combination
+def setUpModule():
+    global driver
+    driver = webdriver.Remote(desired_capabilities=capabilities)
+
+def check_page_title(language, testcenter_url, title):
+    driver.get(testcenter_url + language)
+    assert (driver.title == "Duolingo Test Center")
 
 def test_page_title():
     for language in languages:
         yield check_page_title, language, testcenter_url, "Duolingo Test Center"
 
-def check_page_title(language, testcenter_url, title):
-    pass
+# Check the welcome text for each language as a sanity check to make sure the page is internationalized for the particular
+#def test_page_welcome(self):
+#    languages = ("en", "es", "fr", "de", "it", "pt", "ru", "hi", "hu", "tr")
+#    messages = ("en", "es", "fr", "de", "it", "pt", "ru", "hi", "hu", "tr")
+#    for (language, message) in (languages, messages):
+#        self.driver.get(testcenter_url + language)
 
-    #@unittest.skipIf(1 == 1, "skipped")
-class CheckFrontPage(unittest.TestCase):
+#def test_example2(self):
+#    self.driver.get("http://www.google.com")
+#    self.assertEqual(self.driver.title, "Google")
 
-    # Default browser capabilities
-    #capabilities = { "browserName": "chrome", "platform": "VISTA" }
-    #capabilities = { "browserName": "internet explorer", "platform": "WIN8_1" }
-    capabilities = { "browserName": "firefox", "platform": "VISTA" }
-
-    def setUp(self):
-        self.driver = webdriver.Remote(desired_capabilities=self.capabilities)
-
-    # Make sure the title of the site stays the same regardless of language. This is required for proper branding.
-    def test_page_title(self):
-        for language in languages:
-            self.driver.get(testcenter_url + language)
-            self.assertEqual(self.driver.title, "Duolingo Test Center")
-
-    # Check the welcome text for each language as a sanity check to make sure the page is internationalized for the particular
-    #def test_page_welcome(self):
-    #    languages = ("en", "es", "fr", "de", "it", "pt", "ru", "hi", "hu", "tr")
-    #    messages = ("en", "es", "fr", "de", "it", "pt", "ru", "hi", "hu", "tr")
-    #    for (language, message) in (languages, messages):
-    #        self.driver.get(testcenter_url + language)
-
-    #def test_example2(self):
-    #    self.driver.get("http://www.google.com")
-    #    self.assertEqual(self.driver.title, "Google")
-
-    def tearDown(self):
-        self.driver.quit()
+def tearDownModule():
+    driver.quit()
 
 if __name__ == "__main__":
-    CheckFrontPage.capabilities = {
-      "browserName": sys.argv[1],
-      "platform": sys.argv[2],
-    }
 
-    print "JUST A TEST"
+    print "TESTING: " + browserName + " on " + systemPlatform
+    print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 
     # The unit testing framework takes over the command line arguments, so removing ours to no cause trouble
     del sys.argv[1:]
