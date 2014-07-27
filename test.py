bf0ce4352b7e97181bfb5a41824489e7708f714e
Max Putas <max@duolingo.com>
2014-07-26 23:46:31 -0400

    Added some more language tests

--- test.py
+++ test.py
@@ -3,6 +3,16 @@ from selenium import webdriver
 import unittest
 import sys
 
+testcenter_url = 'http://testcenter.duolingo.com/'
+languages = ("en", "es", "fr", "de", "it", "pt", "ru", "hi", "hu", "tr")
+
+# Make sure the title of the site stays the same regardless of language. This is required for proper branding.
+def test_page_title(self):
+    for language in languages:
+        self.driver.get(testcenter_url + language)
+        self.assertEqual(self.driver.title, "Duolingo Test Center")
+
+    #@unittest.skipIf(1 == 1, "skipped")
 class CheckFrontPage(unittest.TestCase):
 
     # Default browser capabilities
@@ -11,9 +21,22 @@ class CheckFrontPage(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Remote(desired_capabilities=self.capabilities)
 
-    def test_example(self):
-        self.driver.get("http://testcenter.duolingo.com")
-        self.assertEqual(self.driver.title, "Duolingo Test Center")
+    # Make sure the title of the site stays the same regardless of language. This is required for proper branding.
+    def test_page_title(self):
+        for language in languages:
+            self.driver.get(testcenter_url + language)
+            self.assertEqual(self.driver.title, "Duolingo Test Center")
+
+    # Check the welcome text for each language as a sanity check to make sure the page is internationalized for the particular
+    #def test_page_welcome(self):
+    #    languages = ("en", "es", "fr", "de", "it", "pt", "ru", "hi", "hu", "tr")
+    #    messages = ("en", "es", "fr", "de", "it", "pt", "ru", "hi", "hu", "tr")
+    #    for (language, message) in (languages, messages):
+    #        self.driver.get(testcenter_url + language)
+
+    #def test_example2(self):
+    #    self.driver.get("http://www.google.com")
+    #    self.assertEqual(self.driver.title, "Google")
 
     def tearDown(self):
         self.driver.quit()
@@ -27,4 +50,4 @@ if __name__ == "__main__":
     # The unit testing framework takes over the command line arguments, so removing ours to no cause trouble
     del sys.argv[1:]
 
-    unittest.main()
+    unittest.main(verbosity=1)
