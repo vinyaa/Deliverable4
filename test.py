0da5b3ec5db68f5266efc432d44e1f843ad9c264
Max Putas <max@duolingo.com>
2014-07-28 22:19:10 -0400

    Added some more notes

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ from nose.plugins.skip import Skip, SkipTest
 import unittest
 import sys
 import nose
+import time
 
 testcenter_url = 'http://testcenter.duolingo.com/'
 languages = ("en", "es", "fr", "de", "it", "pt", "ru", "hi", "hu", "tr")
@@ -26,7 +27,7 @@ capabilities = {
 def setUpModule():
     global driver
     driver = webdriver.Remote(desired_capabilities=capabilities)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
 
 # Make sure that the page title is the same for each language.
 # This not only makes branding consistent, but is also a sanity
@@ -62,8 +63,9 @@ def test_android_app_link():
 
 def test_chrome_app_link():
 
-    # The testing framework does not yet support the move_to_element method
-    if driver.capabilities['browserName'] == 'safari':
+    # The web driver for Safari does not yet support the move_to_element method and there is currently
+    # a visibility bug in the firefox driver, so this feature cannot be effectively tested on these browsers
+    if (driver.capabilities['browserName'] == "safari" or (driver.capabilities['browserName'] == "firefox")):
         raise SkipTest
 
     driver.get(testcenter_url + 'en')
@@ -71,12 +73,16 @@ def test_chrome_app_link():
     hoverButton = ActionChains(driver).move_to_element(button)
     hoverButton.perform()
     loginBox = driver.find_element_by_xpath('//div[@id="app"]/section[1]/div[1]/div[2]/p')
-    chromeLink = driver.find_element_by_css_selector('.start-chrome').click()
+    chromeLink = driver.find_element_by_css_selector('.start-chrome')
+    wait = WebDriverWait(driver, 30)
+    wait.until(EC.visibility_of(chromeLink))
+    if chromeLink.is_displayed():
+        chromeLink.click()
 
     # Users are redirected to a user creation page if they are not currently logged in when using Chrome
     # If they are using other browsers, they are redirected to a Chrome download page
     if driver.capabilities['browserName'] == 'chrome':
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 30)
         wait.until(EC.visibility_of(loginBox))
         loginText = driver.find_element_by_xpath('//div[@id="app"]/section[1]/div[1]/div[2]/p').text.strip()
         assert (loginText == "You need a Duolingo account to save your test results.")
