a87eadc827d4784e53dcb5163efd14208f264cfd
Max Putas <max@duolingo.com>
2014-07-29 01:57:29 -0400

    Added full run output and some more notes and cleanup

--- startSeleniumServer.sh
+++ startSeleniumServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar selenium-server-standalone-2.42.2.jar -timeout 30 -newSessionWaitTimeout 25000 -role hub &> selenium_server.log &
+java -jar selenium-server-standalone-2.42.2.jar -maxSession 10 -timeout 30 -newSessionWaitTimeout 25000 -role hub &> selenium_server.log &
